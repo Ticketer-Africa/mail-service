a3215d53064f1d6a107a7f2b8a868e77eef7f265
Mayokun Areola <kareola960@gmail.com>
2025-11-08 01:38:36 +0100

    debugging

--- src/mail/mail.worker.ts
+++ src/mail/mail.worker.ts
@@ -1,9 +1,10 @@
+/* eslint-disable @typescript-eslint/no-floating-promises */
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
 /* eslint-disable @typescript-eslint/no-unsafe-member-access */
 /* eslint-disable @typescript-eslint/no-unsafe-call */
-/* eslint-disable @typescript-eslint/no-unsafe-assignment */
-// mail.worker.ts
+
 import { parentPort, workerData } from 'worker_threads';
-import * as nodemailer from 'nodemailer';
+import nodemailer from 'nodemailer';
 
 async function sendMail() {
   const { to, subject, html, from } = workerData;
